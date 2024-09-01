b136db9139fc33598ad08df546435675be3412f4
emrno <osbluespring@gmail.com>
2024-09-01 19:55:38 +0800

    Initial commit

--- app.py
+++ app.py
@@ -7,26 +7,26 @@ app = Flask(__name__)
 
 def get_base_directory():
     system = platform.system()
-    if system == 'Linux' or system == 'Darwin':
-        base_directory = os.path.expanduser('~')
+    if system == "Linux" or system == "Darwin":
+        base_directory = os.path.expanduser("~")
     else:
-        base_directory = '/'
+        base_directory = "/"
     return base_directory
 
 base_directory = get_base_directory()
 current_directory = base_directory
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/execute', methods=['POST'])
+@app.route("/execute", methods=["POST"])
 def execute():
     global current_directory
     data = request.json
-    command = data.get('command')
+    command = data.get("command")
 
-    if command.startswith('cd '):
+    if command.startswith("cd "):
         path = command[3:].strip()
         new_directory = os.path.join(current_directory, path)
         if os.path.isdir(new_directory):
@@ -34,8 +34,10 @@ def execute():
             output = f"\r"
         else:
             output = f"cd: no such file or directory: {path}"
-    elif command == 'clear':
-        output = 'CLEAR'
+    elif command == "clear":
+        output = "CLEAR"
+    elif command == "exit":
+    	output = "EXIT"
     else:
         try:
             result = subprocess.run(command, shell=True, cwd=current_directory, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
@@ -43,7 +45,7 @@ def execute():
         except subprocess.CalledProcessError as e:
             output = e.stderr
 
-    return jsonify({'output': output, 'current_directory': current_directory})
+    return jsonify({"output": output, "current_directory": current_directory})
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
